0dc2067691bdc9d8e16d37415b10697f1472184f
Pedro Meio-Dia <pedro.meiodia@hotmail.fr>
2021-05-07 17:30:26 +0200

    some changes
    Some changes

--- ZTM Python Developer - Projects/Projects/send_sms_with_python/sms_with_python.py
+++ ZTM Python Developer - Projects/Projects/send_sms_with_python/sms_with_python.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
 # To set up environmental variables, see http://twil.io/secure
-account_sid ='ACae9769983f5f1dcc40e982199b54bb95'
-auth_token = '37eb50ff51924d65e54d0ecfac54e588'
+account_sid ='your account_sid'
+auth_token = 'your auth_token'
 # best practice
 # account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -13,8 +13,8 @@ auth_token = '37eb50ff51924d65e54d0ecfac54e588'
 client = Client(account_sid, auth_token)
 
 message = client.api.account.messages.create(
-    to="+number",
-    from_="+number",
-    body="Great work! Click www.lepetitlivre.org")
+    to="+ add number",
+    from_="+ add number",
+    body="Your message")
     
 print(message.sid)
